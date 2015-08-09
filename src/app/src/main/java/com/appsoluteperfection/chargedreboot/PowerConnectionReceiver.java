d6f706e4dcb3fb80f05e8cba11f3508aec262e55
Stephen <raphael.appsoluteperfection@gmail.com>
2015-08-09 12:36:35 -0500

    Basic wireup of service with a listener
    Now to test it all out.

--- src/app/src/main/java/com/appsoluteperfection/chargedreboot/PowerConnectionReceiver.java
+++ src/app/src/main/java/com/appsoluteperfection/chargedreboot/PowerConnectionReceiver.java
@@ -4,11 +4,30 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.os.BatteryManager;
+import android.widget.Toast;
+
+import com.appsoluteperfection.chargedreboot.commands.LogStatusCommand;
+import com.appsoluteperfection.chargedreboot.commands.ResetCommand;
+import com.appsoluteperfection.chargedreboot.queries.StatusSummaryQuery;
+
+import java.io.IOException;
 
 public class PowerConnectionReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-
+        LogStatusCommand logStatusCommand = CommandQueryFactory.getLogStatusCommand(context);
+        logStatusCommand.logCurrentStatus();
+        StatusSummaryQuery statusSummaryQuery = CommandQueryFactory.getStatusSummaryQuery();
+        boolean shouldRestart = statusSummaryQuery.shouldRestart();
+        if (shouldRestart){
+            ResetCommand resetCommand = CommandQueryFactory.getResetCommand();
+            try {
+                resetCommand.resetDevice();
+            } catch (IOException e) {
+                Toast.makeText(context, "Error resetting device: " + e.getMessage(),
+                        Toast.LENGTH_LONG).show();
+            }
+        }
     }
 }
